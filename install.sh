860d6bff6179e4363dd65e82eb621053e7ebcf3e
Aseef MOHAMMED <aseefm25@gmail.com>
2021-02-14 19:44:06 +0530

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,12 @@
 sudo apt install -y maven
 sudo apt install -y openjdk-8-jre-headless
+cd aseef-crio-meme-backend
 mvn install
+cd ..
 sudo apt install -y postgresql postgresql-contrib
 sudo -u postgres createdb mydatabase
-sudo -u postgres psql
-select current_database();
-\c mydatabase
-CREATE TABLE public.meme
-(
+sudo -u postgres -H -- psql -d database_name -c "CREATE TABLE public.meme(id  SERIAL PRIMARY KEY NOT NULL,name text,caption text,url text ) TABLESPACE pg_default;"
 
-    id  SERIAL PRIMARY KEY NOT NULL,
+sudo -u postgres -H -- psql -d database_name -c "ALTER TABLE publicmeme OWNER to postgres;"
 
-    name text COLLATE pg_catalog."default",
-
-    caption text COLLATE pg_catalog."default",
-	url text COLLATE pg_catalog."default"
-
-)
-TABLESPACE pg_default;
-
-ALTER TABLE publicmeme
-
-    OWNER to postgres;
-	
-\q
 
