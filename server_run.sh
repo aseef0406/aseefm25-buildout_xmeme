cc0456df43e97eb2f8c6202543727f3d9e21515e
Aseef MOHAMMED <aseefm25@gmail.com>
2021-02-14 18:09:41 +0000

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,3 +1,7 @@
 cd aseef-crio-meme-backend
 
 java -jar target/aseef-crio-meme-backend-0.0.1-SNAPSHOT.jar
+
+
+echo "If we got a Database Password Fail, It will error from Postgresql configuration /etc/postgresql/10/main/pg_hba.conf"
+echo "Kindly please change the value ident to trust for IPv4 local connections, Thank You"
